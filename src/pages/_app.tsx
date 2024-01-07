02c6d0576c9c4d87fa4071fb12434f98f644c724
lidevlin <lidevlin95@gmail.com>
2024-01-07 23:52:34 +0000

    Added motatmo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { useEffect } from "react";
+import { init } from "@socialgouv/matomo-next";
 import "~/styles/globals.css";
 
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
   return <Component {...pageProps} />;
 };
 
