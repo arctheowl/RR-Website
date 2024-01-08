016edb24460e72305042c6802eec6f64d25acc68
lidevlin <lidevlin95@gmail.com>
2024-01-08 00:09:17 +0000

    Fixed error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { init } from "@socialgouv/matomo-next";
 import "~/styles/globals.css";
 
-const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || "";
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || "";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   useEffect(() => {
